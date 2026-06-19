658229acb7c5ce574f7a720a3b4a36512805718d
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-19 15:23:11 +0530

    code to find the floor of a number in java

---
+++ src/com/jahnvi/floor.java
@@ -0,0 +1,33 @@
+package com.jahnvi;
+
+public class floor {
+    public static void main(String[] args) {
+        int [] arr = { 2 , 3  , 5 , 9 , 14 , 16 , 18};
+        int target = 15;
+
+        // question : to find ceiling of a number:-
+        // point to be remembered is that , whenever sorted array shows up , we have to try binary search in it.
+
+        System.out.println(Ceiling(arr, target));
+    }
+    static int Ceiling(int[] arr , int target){
+        int s = 0;
+        int e = arr.length - 1 ;
+        int mid = s + (e-s)/2;
+
+        while(s<=e){
+            mid = s + (e-s)/2;
+
+            if(target==arr[mid]){
+                return arr[mid];
+            }
+            if(target>arr[mid]){
+                s=mid+1;
+            } else {
+                e=mid-1;
+            }
+        }
+        return e;
+    }
+}
+
