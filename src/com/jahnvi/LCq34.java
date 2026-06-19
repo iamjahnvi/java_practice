75a2b4f2b930dc32fb18499a23be28175bd9c58b
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-20 00:39:05 +0530

    question 34 of LeetCode about finding the first and last position of element in array

--- src/com/jahnvi/LCq34.java
+++ src/com/jahnvi/LCq34.java
@@ -1,12 +1,13 @@
 package com.jahnvi;
+import java.util.Arrays;
 
 public class LCq34 {
     public static void main(String[] args) {
 
-        int[] nums = {5 , 7 , 7 , 8 , 8 , 10} ;
-        int target = 8;
+        int[] nums = {1,1,2,2,2,3,3,3,4,4,4,4,5} ;
+        int target = 4;
 
-        System.out.println(Position(nums, target));
+        System.out.println(Arrays.toString(Position(nums, target)));
     }
 
     static int[] Position(int[] nums , int target){
