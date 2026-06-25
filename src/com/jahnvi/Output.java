0c339ab4c832203f560a18a27145172276d8424d
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-25 15:46:36 +0530

    output in java

---
+++ src/com/jahnvi/Output.java
@@ -0,0 +1,20 @@
+package com.jahnvi;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+
+        // out is a ref var of type printstream with a method in it, called as println. 
+
+        // this println will pass it to valuof function which will further pass it to toString function
+
+
+
+
+
+
+
+
+
+    }
+}
