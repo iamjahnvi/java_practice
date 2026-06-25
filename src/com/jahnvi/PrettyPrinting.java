d002c9f415536a98783c8b17056fee59d88fdfd2
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-26 00:49:14 +0530

    Printing nicely in java, upto few decimal places

---
+++ src/com/jahnvi/PrettyPrinting.java
@@ -0,0 +1,16 @@
+package com.jahnvi;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.12345f;
+        System.out.printf("The formatted number is %.2f" , a);
+
+        System.out.printf("Pie : %.3f" , Math.PI);
+
+
+
+
+
+
+    }
+}
