550e43e7f7d4c94518d6e2c4f6dda29db3fbfc03
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-26 21:16:52 +0530

    Pretty ways of printing in java

--- src/com/jahnvi/PrettyPrinting.java
+++ src/com/jahnvi/PrettyPrinting.java
@@ -6,8 +6,17 @@ public class PrettyPrinting {
         System.out.printf("The formatted number is %.2f" , a);
 
         System.out.printf("Pie : %.3f" , Math.PI);
+        System.out.println();
+        System.out.printf("Hello my name is %s and i am %s " , "kunal" , "cool");
 
 
+        System.out.println('a' + 'b');
+        // shows sum of their ascii values
+
+        System.out.println("a" +"b");
+        // concatenation of characters
+
+        System.out.println('a' + 3);
 
 
 
