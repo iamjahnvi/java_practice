2e1907aeae23df04db6fd1ee51e61416839e9111
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-28 22:17:45 +0530

    Message file, is being created inside packages so as to understand how methods can be implemented on any other file in some other package

---
+++ src/com/packages/Message.java
@@ -0,0 +1,10 @@
+package com.packages;
+
+public class Message {
+    public static void main(String[] args) {
+        
+    }
+    public static void message(){
+        System.out.println("hi i am j :)");
+    }
+}
