062e23f8ad12e867b1b7bac32e22aee6a65d9efe
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-29 20:19:54 +0530

    theory of inheritance

---
+++ src/com/jahnvi/properties/inheritance/InheritanceTheory.java
@@ -0,0 +1,38 @@
+package com.jahnvi.properties.inheritance;
+
+public class InheritanceTheory {
+    public static void main(String[] args) {
+        
+
+        // single inheritance - when one child class , is child to one parent class.
+
+        // multiple inheritance - when the child class itself becomes parent to some other child class.
+
+        // multiple inheritance - one class is extending more than one classes.
+        // not allowed in java
+        // part of interfaces
+
+        // hierarchial inheritance - one class is parent to a lot of child classes
+
+        // hybrid inheritance - combination of single and multipls inheritance 
+        // not in java
+        // part of interfaces
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
