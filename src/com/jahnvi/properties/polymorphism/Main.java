57e45c9fb3420fb4b838c4deb51278fc88184d3b
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-29 23:38:40 +0530

    application of Polymorphism

---
+++ src/com/jahnvi/properties/polymorphism/Main.java
@@ -0,0 +1,13 @@
+package com.jahnvi.properties.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Circle circle = new Circle();
+        Square square = new Square();
+
+        square.area();
+    }
+}
+
+
