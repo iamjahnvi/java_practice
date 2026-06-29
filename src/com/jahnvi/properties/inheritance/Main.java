5e5e042744f1799bd5dad914c86813371363a367
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-06-29 15:06:34 +0530

    application of inheritance

---
+++ src/com/jahnvi/properties/inheritance/Main.java
@@ -0,0 +1,72 @@
+package com.jahnvi.properties.inheritance;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Box box1  = new Box();
+        // box is an object of a constructor named as Box.
+
+        System.out.println(box1.l + " " + box1.w + " " + box1.h);
+        // it will run the constructor with no arguments.
+
+        Box box2 = new Box(4);
+        System.out.println(box2.l + " " + box2.w + " " + box2.h);
+
+        Box box3 = new Box(3.2 , 4.5 , 6.7);
+        System.out.println(box3.l + " " + box3.w + " " + box3.h);
+
+
+        Box box4 = new Box(box1);
+        // an constructor containing another constructor.
+
+
+        BoxWeight bw = new BoxWeight(23.34);
+        System.out.println(bw.weight + " " + bw.l+ "" + bw.w + bw.h);
+
+        // 23.34 -1.0-1.0-1.0
+
+        // also , it is important to note that any instance of box class which is declared as private cannot be accessed anywhere outside the class , itself.
+
+
+        Box object = new BoxWeight(4.5 , 3.2 , 3.4 , 6.7);
+
+        // System.out.println(object.weight);
+
+        System.out.println(object.l);
+        System.out.println(object.w);
+        System.out.println(object.h);
+
+        // we can access the length, width, and height of object but not the weight.
+        // because , it is the type of the ref var(object) , that decides which properties can be accessed and not the type of the object.
+
+        // Boweight object2 = new Box(3.4 , 8.9 , 9.0  , 7.8);
+
+        // here we can see , that it is showing becuase the constructor named as Box cannot access the properties of it's child class, BoxWeight
+
+
+
+
+        // BoxWeight obj3 = new Box(4.5 , 6.7 , 5.6);
+
+        // it is still showing error , why because we are trying to referencing a child class to an obj of parent class.
+
+        // more explaination:-
+        // there are many variables in both parent and child class.
+        // and you are given access to variables of the class , that are in the ref type i.e , BowWeight
+        // hence , you should have access to weight variable
+        // this also means , that the ones you are trying to accesss should be initialised.
+        // but here, when the object itself is of type parent class , how will you call the constructor.
+        // the constructor of parent class is unknown to the properties of it's child class , and it's variables , like weight.
+    
+
+
+
+
+
+
+
+
+
+    }
+}
