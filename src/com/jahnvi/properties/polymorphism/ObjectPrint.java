45172edd911b1ed4e6d675419053c0f0718b1f4c
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-02 23:57:37 +0530

    object printing example

---
+++ src/com/jahnvi/properties/polymorphism/ObjectPrint.java
@@ -0,0 +1,14 @@
+package com.jahnvi.properties.polymorphism;
+
+public class ObjectPrint {
+
+    int num;
+
+    public ObjectPrint(int num){
+        this.num = num;
+    }
+    public static void main(String[] args) {
+        ObjectPrint obj = new ObjectPrint(44);
+        System.out.println(obj);
+    } 
+}
