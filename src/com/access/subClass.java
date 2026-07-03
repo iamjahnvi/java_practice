ae545dfc98fb64f9fecc3f476cae104cd45e9bbd
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-03 23:44:46 +0530

    subclass in java, extending a

---
+++ src/com/access/subClass.java
@@ -0,0 +1,32 @@
+package com.access;
+
+import java.util.Arrays;
+
+public class subClass extends A{
+    
+    public subClass(int num , String name){
+        super(num , name);
+    }
+
+    public subClass(float marks){
+        super(marks);
+    }
+    public static void main(String[] args) {
+        subClass obj = new subClass(45,"kunal");
+        int n = obj.getNum();
+
+        System.out.println(obj instanceof A);
+        // this obj is a object of subclass of A, and subclass extends A, so obj will be instance of A
+        // true
+
+        System.out.println(obj instanceof subClass); 
+        // true
+        
+        System.out.println(obj instanceof Object);
+        // true
+        
+
+
+    }
+}
+
