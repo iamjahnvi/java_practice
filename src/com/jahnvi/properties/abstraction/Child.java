602c2eb423254852f23dab65c2604be236b15fd0
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-07 00:01:42 +0530

    Child class in java, extending Parent, example of babstract class

--- src/com/jahnvi/properties/abstraction/Child.java
+++ src/com/jahnvi/properties/abstraction/Child.java
@@ -13,4 +13,6 @@ public abstract class Child extends Parent{
     }
     // it showed error when i didn;t write abstract in front of return type - void, why?
     // becuase writing abstract keyword means , only declaration of a particular method will be done, with no implemention as we did in parent class.
+
+    // multiple inheritance is not allowed in case of abstrac classes , due to which , a single child class cannot inherit more than one parent class , and this problem is being solved by interfaces, where the methods are defined similar to that of abstract methods, but the only diff is we can inherit multiple classes. also in case of intefaces, the variables used are static(whose object cannot be made)and final in nature(cannot be changed). the keyword used in case of abstarction is extends  , while that in interfaces is implements.
 }
