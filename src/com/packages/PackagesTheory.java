641d103e56484a0fe6a7ceb8c570886203571e1b
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-06 23:56:27 +0530

    List of packages existing in java

---
+++ src/com/packages/PackagesTheory.java
@@ -0,0 +1,31 @@
+package com.packages;
+
+public class PackagesTheory {
+    public static void main(String[] args) {
+        // in java we have the following packages
+        // lang package 
+        // io package 
+        // util package
+        // applet package
+        // awt package
+        // net package
+        
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
