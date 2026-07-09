fc1e1f3ffc80ef7ae1bf80e0f305cdf28f359ee1
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-09 23:14:59 +0530

    methods of media player in java

---
+++ src/com/jahnvi/properties/interfaces/MediaPlayer.java
@@ -0,0 +1,9 @@
+package com.jahnvi.properties.interfaces;
+
+public interface MediaPlayer{
+    void start();
+    void stop();
+
+    // by mistake i defined MediaPlayer as a class, due to which it showed error , becuase then it's body has to be writtened there. but being mthods of interface , there's no need to write their body
+
+}
