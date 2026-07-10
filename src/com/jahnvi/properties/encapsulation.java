f4293120b3e040873004617212ab54d0a6413e9c
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-10 23:33:08 +0530

    Encapsulation in java

---
+++ src/com/jahnvi/properties/encapsulation.java
@@ -0,0 +1,10 @@
+package com.jahnvi.properties;
+
+public class encapsulation {
+    
+    // encapsulation : wrapping up the implemention of data numbers & methods in a class.
+
+    // abstraction is about hiding the unneccesary details and showing valuable information
+
+    
+}
